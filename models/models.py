946d4fd459edf24bd7e18b34bb53b090351dfbff
rahmansaleh7 <rahman.saleh7@gmail.com>
2020-02-10 17:06:58 +0700

    Tabungan Amanah

--- models/models.py
+++ models/models.py
@@ -5,5 +5,6 @@ from odoo import models, fields, api
 class Amanah(models.Model):
     _name = 'kopsyah.amanah'
 
-    name = fields.Char(string="Nama")
+    name = fields.Char(string="No")
+    parent_id = fields.Many2one(string="ID Anggota", comodel_name="res.partner")
     description = fields.Char(string="Deskripsi")
